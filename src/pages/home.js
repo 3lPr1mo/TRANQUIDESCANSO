163458813732c722ab6565a860e7ae76f17db98b
3lPr1mo <erickenriquechaparro@gmail.com>
2023-05-27 20:28:36 -0500

    Sidebar home styled

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import Sidebar from "@/components/Sidebar";
+import Head from "next/head";
+
+export default function home() {
+  return (
+    <>
+      <Head>
+        <title>TRANQUI HOME</title>
+      </Head>
+      <Sidebar/>
+    </>
+  );
+}
